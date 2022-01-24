8f2cf9ccefdbe007e6b160cb9ebdbf99d2ed92c2
Federico Tocco <fedecap26@gmail.com>
2022-01-23 22:52:47 -0300

    integracion webpack, modificado readme

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   entry: "./src/index.js",
   output: {
